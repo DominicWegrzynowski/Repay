ae454cd85eaf2f69eea442d248a4e4ba8d71cf8d
Dominic Wegrzynowski <dominicwegrzynowski@gmail.com>
2021-09-02 12:29:03 -0400

    v3 - finished product

--- js/site.js
+++ js/site.js
@@ -3,74 +3,96 @@ function getData() {
     //gather user input
     let balance = parseFloat(document.getElementById("balance").value);
     let term = parseFloat(document.getElementById("term").value);
-    let interestRate = parseFloat(document.getElementById("interestRate").value);
+    let rate = parseFloat(document.getElementById("interestRate").value);
 
     //make calculations
-    let totalMonthlyPayment = calculateTotalMonthlyPayment(balance, term, interestRate);
-    let startingRemainingBalance = calculateStartingRemainingBalance = balance
-    let interestPayment = calculateInterestPayment(balance, term, interestRate);
-    let principalPayment = calculatePrincipalPayment(balance, term, interestRate);
-    let finalRemainingBalance = calculateFinalRemainingBalance(balance, term, interestRate);
-    let totalPrincipal = balance;
-
-    //write data to the page
-    displayData(paymentAmount, principalPaidThisMonth, interestPaidThisMonth, totalInterestPaidToDate, endOfMonthBalance);
-
+    let calculatedData = calculateData(balance, term, rate);
+    //write data to the page    
+    display(calculatedData);
 }
 
-function calculateTotalMonthlyPayment(balance, term, interestRate) {
+function calculateData(balance, term, rate) {
 
-    let output;
+    let output = { 
+        totalInterestPaid: 0,
+        totalPrincipalPaid: 0,
+        dataArray: [],
+    };
 
-    output = (balance) * ( interestRate / 1200) / (1 - (1 + interestRate / 1200)**(-term) )
-
-    return output;
-
-}
-
-function calculateStartingRemainingBalance(balance, term, interestRate) {
-
-    let output;
-
-
-
-    return output;
-
-}
-
-function calculateInterestPayment(previousRemainingBalance, interestRate) {
-
-    let output;
-
-    output = (previousRemainingBalance * rate / 1200);
-
-    return output;
-
-}
-
-function calculatePrincipalPayment(totalMonthlyPayment, interestPayment) {
-
-    let output;
-
-    output = totalMonthlyPayment - interestPayment;
-
-    return output;
-
-}
-
-function calculateFinalRemainingBalance(balance, term, interestRate) {
-
-    let output;
-
+    let previousRemainingBalance = balance;
+  
     
+    for (let month = 1; month <= term; month++) {
+        
+        let totalMonthlyPayment = (balance) * (rate / 1200) / (1 - (1 + rate / 1200)**(-term));
+
+        let interestPayment = previousRemainingBalance * rate / 1200;
+        output.totalInterestPaid += interestPayment;
+
+        let principalPayment = totalMonthlyPayment - interestPayment;
+        output.totalPrincipalPaid += principalPayment;
+
+        let remainingBalance = previousRemainingBalance - principalPayment;
+        previousRemainingBalance = remainingBalance;
+
+        if(remainingBalance < 0) {
+            remainingBalance = 0;
+        }
+
+        output.dataArray.push(month);
+        output.dataArray.push(formatToCurrency(totalMonthlyPayment));
+        output.dataArray.push(formatToCurrency(principalPayment));
+        output.dataArray.push(formatToCurrency(interestPayment));
+        output.dataArray.push(formatToCurrency(output.totalInterestPaid));
+        output.dataArray.push(formatToCurrency(remainingBalance));
+        
+    }
 
     return output;
+}
+function formatToCurrency(amount) {
+
+    return (amount).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,'); 
 
 }
 
-function displayData() {
 
-    document.getElementById("totalPrincipalLabel").value = totalPrincipal;
-    document.getElementById("totalInterestLabel").value = totalInterestPaidToDate;
+function display(output) {
+
+    //financial variables
+    totalPrincipalPaid = output.totalPrincipalPaid;
+    totalInterestPaid = output.totalInterestPaid;
+    totalCost = totalPrincipalPaid + totalInterestPaid;
+    dataArray = output.dataArray;
+    
+    if(dataArray[1] == undefined) {
+        document.getElementById("monthlyPaymentLabel").innerHTML = '$' + formatToCurrency(0)
+    } 
+    else {
+        document.getElementById("monthlyPaymentLabel").innerHTML = '$' + dataArray[1];
+    } 
+
+    document.getElementById("totalPrincipalLabel").innerHTML = '$' + formatToCurrency(totalPrincipalPaid);
+    document.getElementById("totalInterestLabel").innerHTML = '$' + formatToCurrency(totalInterestPaid);
+    document.getElementById("totalCostLabel").innerHTML = '$' + formatToCurrency(totalCost);
+    
+    //DOM variables
+    let tableBody = document.getElementById("results");
+    let templateRow = document.getElementById("repayTemplate");
+
+    for(let i = 0; i < dataArray.length; i += 6) {
+
+        let tableRow = document.importNode(templateRow.content, true);
+        let rowCols = tableRow.querySelectorAll("td");
+
+        rowCols[0].textContent = dataArray[i];
+        rowCols[1].textContent = dataArray[i + 1];
+        rowCols[2].textContent = dataArray[i + 2];
+        rowCols[3].textContent = dataArray[i + 3];
+        rowCols[4].textContent = dataArray[i + 4];
+        rowCols[5].textContent = dataArray[i + 5];
+
+        tableBody.appendChild(tableRow);
+    }
 
 }
